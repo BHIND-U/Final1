cc38bd7e33bc014788e3fe8407580e4fd45c3cf0
BHIND-U <157286798+BHIND-U@users.noreply.github.com>
2024-03-11 05:02:42 -0700

    Update Program.cs

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,8 +1,8 @@
-﻿/*
- *Author: Joshua Isidro
- * Purpose: Code for final
- * Course: COMP003A
- */
+/*
+* Author: Joshua Isidro
+* Purpose: Code for final
+* Course: COMP003A
+*/
 
 using System;
 using System.Text.RegularExpressions;
@@ -17,40 +17,57 @@ namespace Final
         static void Main(string[] args)
         {
             Console.WriteLine("New Patient Form");
-            Console.WriteLine("In order to provide the best medical care we will need information about yourself, please answer truthfully");
+            Console.WriteLine("In order to provide the best medical care we will need information about yourself, please answer truthfully.");
             Console.WriteLine("");
-            Info();
-            Console.WriteLine($"Hello {First} {Last}, you chose {Gender} as your gender and you are {Age} years old");
-        }
-
-        static void Info()
-        {
+            
+            
             do
             {
                 Console.Write("First Name: ");
                 First = Console.ReadLine();
             } while (!Letters(First));
 
+            
             do
             {
                 Console.Write("Last Name: ");
                 Last = Console.ReadLine();
             } while (!Letters(Last));
 
-            do
-            {
-                Console.Write("Gender (M/F/O): ");
-                Gender = Console.ReadLine().ToUpper();
-            } while (Gender != "M" && Gender != "F" && Gender != "O");
+            
+            Console.Write("Gender (M/F/O): ");
+            Gender = Console.ReadLine().ToUpper();
 
+            
+            string GenderDescription;
+            switch (Gender)
+            {
+                case "M":
+                    GenderDescription = "Male";
+                    break;
+                case "F":
+                    GenderDescription = "Female";
+                    break;
+                case "O":
+                    GenderDescription = "Other";
+                    break;
+                default:
+                    GenderDescription = "Other not listed";
+                    break;
+            }
+
+            
             Age = GetAge();
+            Console.WriteLine($"Hello {First} {Last}, you are a {GenderDescription} and {Age} years old");
         }
 
+        
         static bool Letters(string input)
         {
             return Regex.IsMatch(input, @"^[a-zA-Z]+$");
         }
 
+        
         static int GetAge()
         {
             int age;
@@ -58,7 +75,6 @@ namespace Final
             {
                 Console.Write("What year were you born? ");
             } while (!int.TryParse(Console.ReadLine(), out age) || age < 1900 || age > DateTime.Now.Year);
-
             return DateTime.Now.Year - age;
         }
     }
